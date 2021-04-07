12c91cf285447d132a6c2de49821b0afb12d93c7
Alejandro <alesantin012@hotmail.com>
2021-04-07 12:58:22 -0300

    Image

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,4 +1,5 @@
 import {useRouter} from 'next/router';
+import Image from 'next/image';
 import Layout from '../../components/layout';
 import Title from '../../components/title';
 
@@ -13,6 +14,7 @@ export default function User({user}) {
             <Title>User ID {user.id}</Title>
             <div className='card'>
                 <h3>User</h3>
+                <Image src={`/images/${user.id}.jpg`} width={400} height={600} objectFit="cover"/>
                 <p>Name: {user.name}</p>
                 <p>Email: {user.email}</p>
                 <p>Phone: {user.phone}</p>
